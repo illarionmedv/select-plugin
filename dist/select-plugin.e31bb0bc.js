377dd465fd1ac9e16f0d2c81e71657211df27d75
illarionmedv <lars200020@gmail.com>
2020-07-27 01:51:46 +0300

    Add methods

--- dist/select-plugin.e31bb0bc.js
+++ dist/select-plugin.e31bb0bc.js
@@ -127,11 +127,137 @@ exports.Select = void 0;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Select = function Select() {
-  _classCallCheck(this, Select);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _classPrivateMethodGet(receiver, privateSet, fn) { if (!privateSet.has(receiver)) { throw new TypeError("attempted to get private field on non-instance"); } return fn; }
+
+var getTemplate = function getTemplate() {
+  var data = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+  var placeholder = arguments.length > 1 ? arguments[1] : undefined;
+  var selectedId = arguments.length > 2 ? arguments[2] : undefined;
+  var text = placeholder !== null && placeholder !== void 0 ? placeholder : 'Placeholder по умолчанию';
+  var items = data.map(function (item) {
+    var cls = '';
+
+    if (item.id === selectedId) {
+      text = item.value;
+      cls = 'selected';
+    }
+
+    return "\n        <li class=\"select__item ".concat(cls, "\" data-type=\"item\" data-id=\"").concat(item.id, "\">").concat(item.value, "</li>\n      ");
+  });
+  return "\n      <div class=\"select__backdrop\" data-type=\"backdrop\"></div>\n      <div class=\"select__input\" data-type=\"input\">\n        <span data-type=\"value\">".concat(text, "</span>\n        <i class=\"fa fa-chevron-down\" data-type=\"arrow\"></i>\n      </div>\n      <div class=\"select__dropdown\">\n        <ul class=\"select__list\">\n          ").concat(items.join(''), "\n        </ul>\n      </div>\n    ");
 };
 
+var _render = new WeakSet();
+
+var _setup = new WeakSet();
+
+var Select = /*#__PURE__*/function () {
+  function Select(selector, options) {
+    _classCallCheck(this, Select);
+
+    _setup.add(this);
+
+    _render.add(this);
+
+    this.$el = document.querySelector(selector);
+    this.options = options;
+    this.selectedId = options.selectedId;
+
+    _classPrivateMethodGet(this, _render, _render2).call(this);
+
+    _classPrivateMethodGet(this, _setup, _setup2).call(this);
+  }
+
+  _createClass(Select, [{
+    key: "clickHandler",
+    value: function clickHandler(event) {
+      var type = event.target.dataset.type;
+
+      if (type === 'input') {
+        this.toggle();
+      } else if (type === 'item') {
+        var id = event.target.dataset.id;
+        this.select(id);
+      } else if (type === 'backdrop') {
+        this.close();
+      }
+    }
+  }, {
+    key: "select",
+    value: function select(id) {
+      this.selectedId = id;
+      this.$value.textContent = this.current.value;
+      this.$el.querySelectorAll('[data-type="item"]').forEach(function (el) {
+        el.classList.remove('selected');
+      });
+      this.$el.querySelector("[data-id=\"".concat(id, "\"]")).classList.add('selected');
+      this.options.onSelect ? this.options.onSelect(this.current) : null;
+      this.close();
+    }
+  }, {
+    key: "toggle",
+    value: function toggle() {
+      this.isOpen ? this.close() : this.open();
+    }
+  }, {
+    key: "open",
+    value: function open() {
+      this.$el.classList.add('open');
+      this.$arrow.classList.remove('fa-chevron-down');
+      this.$arrow.classList.add('fa-chevron-up');
+    }
+  }, {
+    key: "close",
+    value: function close() {
+      this.$el.classList.remove('open');
+      this.$arrow.classList.add('fa-chevron-down');
+      this.$arrow.classList.remove('fa-chevron-up');
+    }
+  }, {
+    key: "destroy",
+    value: function destroy() {
+      this.$el.removeEventListener('click', this.clickHandler);
+      this.$el.innerHTML = '';
+    }
+  }, {
+    key: "isOpen",
+    get: function get() {
+      return this.$el.classList.contains('open');
+    }
+  }, {
+    key: "current",
+    get: function get() {
+      var _this = this;
+
+      return this.options.data.find(function (item) {
+        return item.id === _this.selectedId;
+      });
+    }
+  }]);
+
+  return Select;
+}();
+
 exports.Select = Select;
+
+var _render2 = function _render2() {
+  var _this$options = this.options,
+      placeholder = _this$options.placeholder,
+      data = _this$options.data;
+  this.$el.classList.add('select');
+  this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId);
+};
+
+var _setup2 = function _setup2() {
+  this.clickHandler = this.clickHandler.bind(this);
+  this.$el.addEventListener('click', this.clickHandler);
+  this.$arrow = this.$el.querySelector('[data-type="arrow"]');
+  this.$value = this.$el.querySelector('[data-type="value"]');
+};
 },{}],"C:/Users/illar/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
@@ -211,7 +337,34 @@ var _select = require("./select/select");
 
 require("./select/styles.scss");
 
-var select = new _select.Select();
+var select = new _select.Select('#select', {
+  placeholder: 'Выбери пожалуйста элемент',
+  // selectedId: '2',
+  data: [{
+    id: '1',
+    value: 'React'
+  }, {
+    id: '2',
+    value: 'Angular'
+  }, {
+    id: '3',
+    value: 'Vue'
+  }, {
+    id: '4',
+    value: 'React Native'
+  }, {
+    id: '5',
+    value: 'Next'
+  }, {
+    id: '6',
+    value: 'Nest'
+  }],
+  onSelect: function onSelect(item) {
+    console.log('Selected Item', item);
+  }
+}); // select.select('4')
+
+window.s = select;
 },{"./select/select":"select/select.js","./select/styles.scss":"select/styles.scss"}],"C:/Users/illar/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
